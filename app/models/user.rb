086c2c3f6f95deec238047705abce67c4c2a9c72
Shelby <shelbcoleman@gmail.com>
2017-07-25 18:10:52 +0000

    Adding roles for admin/member

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     
     before_save { self.email = email.downcase if email.present? }
     before_save :capitalize_user
+    before_save { self.role ||= :member }
+    
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
     validates :password, length: { minimum: 6 }, allow_blank: true
@@ -11,6 +13,8 @@ class User < ActiveRecord::Base
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
     has_secure_password
+    
+    enum role: [:member, :admin]
     
     def capitalize_user
         temporary_name = []
