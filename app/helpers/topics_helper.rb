1cfe96473216dfd542528e297920281a30d68e46
Shelby <shelbcoleman@gmail.com>
2017-08-01 20:29:50 +0000

    Hiding some buttons based on roles

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,8 @@ module TopicsHelper
    def user_is_authorized_for_topics?
         current_user && (current_user.admin? || current_user.moderator?)
    end
+   
+   def user_is_authorized_for_topics_admin?
+        current_user && current_user.admin? 
+   end
 end
