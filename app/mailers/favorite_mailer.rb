ee3c1c533e88d647734279bd8ac947d7197339f3
Shelby <shelbcoleman@gmail.com>
2017-08-03 21:18:41 +0000

    Faves after creating post

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,14 +2,24 @@ class FavoriteMailer < ApplicationMailer
     default from: "shelbcoleman@gmail.com"
     
     def new_comment(user, post, comment)
-     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     headers["Message-ID"] = "<comments/#{comment.id}@https://obscure-anchorage-28821.herokuapp>"
+     headers["In-Reply-To"] = "<post/#{post.id}@https://obscure-anchorage-28821.herokuapp>"
+     headers["References"] = "<post/#{post.id}@https://obscure-anchorage-28821.herokuapp>"
  
      @user = user
      @post = post
      @comment = comment
 
      mail(to: user.email, subject: "New comment on #{post.title}")
+    end
+    
+    def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@https://obscure-anchorage-28821.herokuapp>"
+     headers["In-Reply-To"] = "<post/#{post.id}@https://obscure-anchorage-28821.herokuapp>"
+     headers["References"] = "<post/#{post.id}@https://obscure-anchorage-28821.herokuapp>"
+ 
+     @post = post
+
+     mail(to: post.user.email, subject: "New post #{post.title}")
     end
 end
