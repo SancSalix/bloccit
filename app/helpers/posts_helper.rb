a5b2895debd61f29ea85fff8fe055a2d2e4b2aa8
Shelby <shelbcoleman@gmail.com>
2017-08-10 20:55:57 +0000

    Fixing error

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
         current_user && (current_user == post.user || current_user.admin? || current_user.moderator?)
    end
    
-   def user_is_authorized_for_post_admin?
+   def user_is_authorized_for_post_admin?(post)
         current_user && (current_user == post.user || current_user.admin?)
    end
 end
